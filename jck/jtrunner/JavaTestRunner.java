6b3bf10dc0e8de518b08973b06c3618b7b52f911
sobirada <58551844+sobirada@users.noreply.github.com>
2022-11-08 11:48:49 +0530

    Update JavaTestRunner.java

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -695,7 +695,7 @@ public class JavaTestRunner {
 					fileContent += "set jck.env.runtime.remoteAgent.passiveHost localhost" + ";\n";
 					fileContent += "set jck.env.runtime.remoteAgent.passivePortDefault Yes" + ";\n";
 					
-					 if (( tests.contains("vm/jdwp")) && (tests.contains("jck8"))){
+					 if (( tests.contains("vm/jdwp")) && (jckVersion.contains("jck8"))){
 					
 				 	fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
 					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
