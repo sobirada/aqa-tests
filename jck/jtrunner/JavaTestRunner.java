a5893f16bf9d4443dae8a3216ebc1c36649d567a
sobirada <58551844+sobirada@users.noreply.github.com>
2022-11-24 13:46:11 +0530

    Update JavaTestRunner.java

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -697,9 +697,9 @@ public class JavaTestRunner {
 					
 					 if ( tests.contains("vm/jdwp")){
 					
-				 	fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
-					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
-					}
+				 	// fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
+					// fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
+					// }
 				}
 			}
 			// Without the following override the following failures occur:
