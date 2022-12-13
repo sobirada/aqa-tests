dbf3aa50b952afe41e99e5c3ae609bfb746814b7
sobirada <58551844+sobirada@users.noreply.github.com>
2022-12-13 12:38:21 +0530

    Updating concurrency value to 1 for jdwp tests

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -696,7 +696,7 @@ public class JavaTestRunner {
 					fileContent += "set jck.env.runtime.remoteAgent.passivePortDefault Yes" + ";\n";
 					
 					 if ( tests.contains("vm/jdwp")){
-					fileContent += "timeoutfactor 6" + ";\n";	// 6 base time limit equal 60 minutes	 
+					fileContent += "concurrency 1" + ";\n";	
 					
 				 	// fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
 					// fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
